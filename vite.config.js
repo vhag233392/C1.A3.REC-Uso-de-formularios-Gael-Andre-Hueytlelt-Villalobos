cd118b4fd6933152dde84c1eedbab175ca6ef81e
vhag233392 <233392@ids.upchiapas.edu.mx>
2024-07-26 22:15:10 -0600

    Deploy3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://vhag23339.github.io/C1.A3.REC-Uso-de-formularios-Gael-Andre-Hueytlelt-Villalobos/',
+  base:'https://vhag233392.github.io/C1.A3.REC-Uso-de-formularios-Gael-Andre-Hueytlelt-Villalobos/',
 })
